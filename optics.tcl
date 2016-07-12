6069a7318897ce03ed03de745e45a7d6ddd3039e
Zach Conn <zachery.conn@flightaware.com>
2016-07-12 21:22:44 +0000

    More robust error checking; namespace exports.

--- optics.tcl
+++ optics.tcl
@@ -108,6 +108,8 @@
 #
 
 namespace eval ::tcloptics {
+    namespace export lens key index lens_compose lens_view lens_set
+
     proc lens {args} {
         return $args
     }
@@ -133,6 +135,10 @@ namespace eval ::tcloptics {
 
         foreach target $lens {
             if {$target == "each"} {
+                if {![_islist $d]} {
+                    error "lens_view shape incompatibility (expected a list!); attempted to apply lens '$lens' to '$d'."
+                }
+
                 set accumulator [list]
 
                 foreach v $d {
@@ -141,6 +147,10 @@ namespace eval ::tcloptics {
 
                 return $accumulator
             } elseif {$target == "values"} { 
+                if {![_isdict $d]} {
+                    error "lens_view shape incompatibility (expected a dict!); attempted to apply lens '$lens' to $d'."
+                }
+
                 set accumulator [list]
 
                 dict for {k v} $d {
@@ -149,6 +159,10 @@ namespace eval ::tcloptics {
 
                 return $accumulator
             } elseif {$target == "keys"} {
+                if {![_isdict $d]} {
+                    error "lens_view shape incompatibility (expected a dict!); attempted to apply lens '$lens' to $d'."
+                }
+
                 return [dict keys $d]
             } else {
                 if {[llength $target] != 2} {
@@ -157,12 +171,20 @@ namespace eval ::tcloptics {
 
                 switch [lindex $target 0] {
                     "index" {
+                        if {![_islist $d]} {
+                            error "lens_set shape incompatibility (expected a list!); attempted to apply lens '$lens' to '$d'."
+                        }
+
                         set idx [lindex $target 1]
                         set nestedLayer [lindex $d $idx]
                         return [lens_view nestedLayer [lrange $lens 1 end]]
                     }
 
                     "key" {
+                        if {![_isdict $d]} {
+                            error "lens_view shape incompatibility (expected a dict!); attempted to apply lens '$lens' to $d'."
+                        }
+
                         set key [lindex $target 1]
                         set nestedLayer [dict get $d $key]
                         return [lens_view nestedLayer [lrange $lens 1 end]]
@@ -187,25 +209,45 @@ namespace eval ::tcloptics {
             set target [lindex $lens 0]
 
             if {$target == "each"} {
+                if {![_islist $d]} {
+                    error "lens_set shape incompatibility (expected a list!); attempted to apply lens '$lens' to '$d'."
+                }
+
                 set idx 0
                 foreach nestedLayer $d {
                     lset d $idx $v
                     incr idx
                 }
             } elseif {$target == "values"} {
+                if {![_isdict $d]} {
+                    error "lens_set shape incompatibility (expected a dict!); attempted to apply lens '$lens' to '$d'."
+                }
+
                 dict for {k v2} $d {
                     dict set d $k $v
                 }
             } elseif {$target == "keys"} {
                 error "lens_set does not support the traversal lens 'keys': '$lens'."
             } else {
+                if {[llength $target] != 2} {
+                    error "lens_set encountered an ill-formed lens: '$lens'; specifically, '$target'."
+                }
+
                 switch [lindex $target 0] {
                     "index" {
+                        if {![_islist $d]} {
+                            error "lens_set shape incompatibility (expected a list!); attempted to apply lens '$lens' to '$d'."
+                        }
+
                         set idx [lindex $target 1]
                         lset d $idx $v
                     }
 
                     "key" {
+                        if {![_isdict $d]} {
+                            error "lens_set shape incompatibility (expected a dict!); attempted to apply lens '$lens' to '$d'."
+                        }
+
                         set key [lindex $target 1]
                         dict set d $key $v
                     }
@@ -217,6 +259,10 @@ namespace eval ::tcloptics {
 
         foreach target $lens {
             if {$target == "each"} {
+                if {![_islist $d]} {
+                    error "lens_set shape incompatibility (expected a list!); attempted to apply lens '$lens' to '$d'."
+                }
+
                 set idx 0
                 foreach nestedLayer $d {
                     lens_set nestedLayer [lrange $lens 1 end] $v
@@ -226,6 +272,10 @@ namespace eval ::tcloptics {
 
                 return
             } elseif {$target == "values"} {
+                if {![_isdict $d]} {
+                    error "lens_set shape incompatibility (expected a dict!); attempted to apply lens '$lens' to '$d'."
+                }
+
                 dict for {k v2} $d {
                     set nestedLayer [dict get $d $k]
                     lens_set nestedLayer [lrange $lens 1 end] $v
@@ -236,8 +286,16 @@ namespace eval ::tcloptics {
             } elseif {$target == "keys"} {
                 error "lens_set does not support the traversal lens 'keys': '$lens'."
             } else {
+                if {[llength $target] != 2} {
+                    error "lens_set encountered an ill-formed lens: '$lens'; specifically, '$target'."
+                }
+
                 switch [lindex $target 0] {
                     "index" {
+                        if {![_islist $d]} {
+                            error "lens_set shape incompatibility (expected a list!); attempted to apply lens '$lens' to '$d'."
+                        }
+
                         set idx [lindex $target 1]
                         set nestedLayer [lindex $d $idx]
                         lens_set nestedLayer [lrange $lens 1 end] $v
@@ -246,6 +304,10 @@ namespace eval ::tcloptics {
                     }
 
                     "key" {
+                        if {![_isdict $d]} {
+                            error "lens_set shape incompatibility (expected a dict!); attempted to apply lens '$lens' to '$d'."
+                        }
+
                         set key [lindex $target 1]
                         set nestedLayer [dict get $d $key]
                         lens_set nestedLayer [lrange $lens 1 end] $v
@@ -259,6 +321,14 @@ namespace eval ::tcloptics {
                 }
             }
         }
+    }
+
+    proc _isdict {v} {
+        expr {![catch {dict size $v}]}
+    }
+
+    proc _islist {v} {
+        return [string is list -strict $v]
     }
 }
 
